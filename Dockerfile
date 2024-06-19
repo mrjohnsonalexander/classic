51e68bf4daa3b00311dbc16321697b5ad4b85d6c
alexander <mr.johnson.alexander@gmail.com>
2024-06-19 12:25:33 -0700

    Health prob

--- Dockerfile
+++ Dockerfile
@@ -26,5 +26,7 @@ ARG KAGGLE_USERNAME
 ARG KAGGLE_KEY
 ENV KAGGLE_USERNAME=$KAGGLE_USERNAME
 ENV KAGGLE_KEY=$KAGGLE_KEY
+RUN python3.11 -m pip install fastapi==0.111.0
+EXPOSE 4000
 COPY main.py /usr/share/applications/main.py
-CMD ["python3.11", "/usr/share/applications/main.py"]
+CMD ["python3.11", "-m", "uvicorn", "--app-dir", "/usr/share/applications", "main:app", "--reload", "--host", "0.0.0.0", "--port", "4000"]
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+import logging
 import tensorflow
 from tensorflow import keras
 import keras_nlp
 import kaggle
 from kaggle.api.kaggle_api_extended import KaggleApi
+import logging
 import os
+from pydantic import BaseModel
 
 
 api = KaggleApi()
@@ -14,8 +19,51 @@ os.environ["KERAS_BACKEND"] = "tensorflow"
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "1.00"
 keras.config.set_floatx("float16")
 gemma_lm = keras_nlp.models.GemmaCausalLM.from_preset("gemma_1.1_instruct_2b_en", load_weights=False)
-print(f'GemmaCasualLM generate: {gemma_lm.generate("What is the meaning of life?", max_length=30)}')
 
-gpu_devices = tensorflow.config.list_physical_devices('GPU')
-print(tensorflow.config.experimental.get_device_details(gpu_devices[0]))
-print(tensorflow.config.experimental.get_memory_info(f'{gpu_devices[0].device_type}:0'))
+app = FastAPI()
+
+logging.basicConfig(filename='main.log', level=logging.INFO)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class HealthCheck(BaseModel):
+      """Response model to validate and return when performing a health check."""
+
+      status: str = "OK"
+
+@app.get(
+     "/health",
+     tags=["healthcheck"],
+     summary="Perform a Health Check",
+     response_description="Return HTTP Status Code 200 (OK)",
+     status_code=status.HTTP_200_OK,
+     response_model=HealthCheck,
+)
+
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+       Returns:
+          HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
+
+@app.get("/generate")
+async def generate():
+    
+    try:
+        generated = gemma_lm.generate("What is the meaning of life?", max_length=30)
+    except:
+        logger.info("Logged generation error")
+
+    return generated
